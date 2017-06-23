7fc1d872e13a0ba3cb99ef40c6f952495b18d1f8
Rakhal <rakhal@justbetter.nl>
2017-06-23 10:59:07 +0200

    Get storeconfig for use_street2_as_housenumber, not configurable in backend in magento

--- Helper/Data.php
+++ Helper/Data.php
@@ -66,7 +66,7 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper{
 
 		$settings = [
 					//"baseUrl"=> htmlspecialchars($baseUrl),
-					"useStreet2AsHouseNumber"=> true,
+					"useStreet2AsHouseNumber"=> (boolean)$this->_getStoreConfig('postcodenl_api/advanced_config/use_street2_as_housenumber'),
 					"useStreet3AsHouseNumberAddition"=> $this->_getConfigBoolString('postcodenl_api/advanced_config/use_street3_as_housenumber_addition'),
 					"neverHideCountry"=> $this->_getConfigBoolString('postcodenl_api/advanced_config/never_hide_country'),
 					"debug"=> False,
