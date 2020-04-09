05ab15ef852e1af4e9dc93df3562442c3560beb7
Experius <info@experius.nl>
2020-04-09 15:39:34 +0200

    [BUGFIX] removed region_id set, expects an int not a string.
    Removed region_id set because it is not a required field for NL adresses and Magento expects an int not a string.

--- view/frontend/web/js/view/form/postcode.js
+++ view/frontend/web/js/view/form/postcode.js
@@ -412,7 +412,6 @@ define([
                         self.debug('address on single line');
                     }
                     registry.get(self.parentName + '.country_id').set('value', 'NL').set('error', false);
-                    registry.get(self.parentName + '.region_id').set('value', response.province).set('error', false);
                     registry.get(self.parentName + '.city').set('value', response.city).set('error', false);
                     registry.get(self.parentName + '.postcode').set('value', response.postcode).set('error', false);
 
